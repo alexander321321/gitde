1bc90e2e2d5d0b0b6dc9cd599eda6764e8692cd4
alexander321321 <treesome@yandex.ru>
2020-09-19 22:24:31 +0200

    sec.js

---
+++ sec.js
@@ -0,0 +1,19 @@
+let sec = 'sec'
+let sec = 'sec'
+let sec = 'sec'
+let sec = 'sec'
+let sec = 'sec'
+let sec = 'sec'
+let sec = 'sec'
+
+import React, {Component} from 'react'
+
+export default class  extends Component {
+  render() {
+    return {
+      <div>
+        sec
+      </div>
+    }
+  }
+}
